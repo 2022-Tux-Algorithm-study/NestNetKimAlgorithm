d9fbb80e70a0f39dc41201a1277e3542302c4693
jin0825 <74577764+jin0825@users.noreply.github.com>
2022-08-23 22:33:38 +0900

    22.08.23
    보물

---
+++ Baekjoon/1026.py
@@ -0,0 +1,14 @@
+# 보물
+
+n = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+total = 0
+
+a.sort()
+b.sort(reverse=True)
+
+for i in range(n):
+  total += a[i] * b[i]
+
+print(total)
